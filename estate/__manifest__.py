0da1fff48c67be58c26f6c57f88be056f2f5c5ba
Laura Schauer <lasc@odoo.com>
2022-06-01 10:03:27 +0200

    Chapters 1, 2, 3, 4

---
+++ estate/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    'name': 'Real Estate',
+    'version': '1.0',
+    'depends': [
+        'base_setup',
+        'sales_team',
+        'mail',
+        'calendar',
+        'resource',
+        'fetchmail',
+        'utm',
+        'web_tour',
+        'contacts',
+        'digest',
+        'phone_validation',
+    ],
+    'application': True,
+}
---
+++ estate/models/estate_property.py
@@ -0,0 +1,23 @@
+from typing_extensions import Required
+from odoo import models, fields
+
+
+class EstateProperty(models.Model):
+    _name = "estate.property"
+    _description = "This is the description for the estate property."
+
+    name = fields.Char('Name', required=True)
+    description = fields.Text('Description')
+    postcode = fields.Char('Postcode')
+    date_availability = fields.Date('Date Availability')
+    expected_price = fields.Float('Expected Price', required=True)
+    selling_price = fields.Float('Selling Price')
+    bedrooms = fields.Integer('Number Bedrooms')
+    living_area = fields.Integer('Living Area')
+    facades = fields.Integer('Facades')
+    garage = fields.Boolean('Garage')
+    garden = fields.Boolean('Garden')
+    garden_area = fields.Integer('Garden Area')
+    garden_orientation = fields.Selection(
+        string='Type',  # string is the label of the field seen by the user
+        selection=[('north', 'North'), ('east', 'East'), ('south', 'South'), ('west', 'West')])

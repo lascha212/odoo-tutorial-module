84d80178810b4912235daec0719681b4e0cb45e1
Laura <laura.schauer212@gmail.com>
2022-06-01 14:36:28 +0200

    Chapter 6: default values

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -8,15 +8,15 @@ class EstateProperty(models.Model):
     name = fields.Char('Name', required=True)
     description = fields.Text('Description')
     postcode = fields.Char('Postcode')
-    date_availability = fields.Date('Date Availability', copy=False)
+    date_availability = fields.Date('Date Availability', copy=False, default=lambda self: fields.Date.today())
     expected_price = fields.Float('Expected Price', required=True)
     selling_price = fields.Float('Selling Price', readonly=True, copy=False)
-    bedrooms = fields.Integer('Number Bedrooms')
+    bedrooms = fields.Integer('Number Bedrooms', default=2)
     living_area = fields.Integer('Living Area')
     facades = fields.Integer('Facades')
     garage = fields.Boolean('Garage')
     garden = fields.Boolean('Garden')
     garden_area = fields.Integer('Garden Area')
     garden_orientation = fields.Selection(
-        string='Type',  # string is the label of the field seen by the user
+        string='Garden Orientation',  # string is the label of the field seen by the user
         selection=[('north', 'North'), ('east', 'East'), ('south', 'South'), ('west', 'West')])

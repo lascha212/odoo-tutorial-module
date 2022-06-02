cbce68c7118c7487d1949e1cdbb658c7fb87db10
Laura <laura.schauer212@gmail.com>
2022-06-02 13:43:04 +0200

    fixing sequential loading error

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -17,8 +17,8 @@
     'application': True,
     'data': [
         'security/ir.model.access.csv',
-        'views/estate_property_views.xml',
         'views/estate_property_type_views.xml',
+        'views/estate_property_views.xml',
         'views/estate_menus.xml'
     ]
 }

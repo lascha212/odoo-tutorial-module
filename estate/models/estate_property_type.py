4f3dcd155a6825ebfacaa1358f9a98867d7ad9a8
Laura Schauer <lasc@odoo.com>
2022-06-02 15:02:00 +0200

    fixing mistakes

--- estate/models/estate_property_type.py
+++ estate/models/estate_property_type.py
@@ -1,8 +1,8 @@
 from odoo import models, fields
 
 
-class EstateProperty(models.Model):
+class EstatePropertyType(models.Model):
     _name = "estate.property.type"
     _description = "This is the description of the estate property type model."
 
-    name = fields.Char('Name', required=True)    
+    name = fields.Char('Name', required=True)

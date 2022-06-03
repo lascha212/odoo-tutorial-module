782ad86e8febecd0ca602b3abede90105b14d3bf
Laura <laura.schauer212@gmail.com>
2022-06-03 13:23:01 +0200

    polished code

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -21,6 +21,6 @@
         'views/estate_property_tag_views.xml',
         'views/estate_property_type_views.xml',
         'views/estate_property_views.xml',
-        'views/estate_menus.xml'
+        'views/estate_property_menus.xml'
     ]
 }
